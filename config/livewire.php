acb1b0ddb49c6f913b69d8a6a69dd83e71c546aa
kisaragiarya <lorpesarya@gmail.com>
2025-10-04 21:02:29 +0800

    Fix broken login and admin page

--- config/livewire.php
+++ config/livewire.php
@@ -3,19 +3,9 @@
 return [
     'temporary_file_upload' => [
         'disk' => 'local',
-        'rules' => 'required|max:102400', // 100MB max
+        'rules' => null,
         'directory' => 'livewire-tmp',
         'middleware' => null,
-        'preview_mimes' => [
-            'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
-            'mov', 'avi', 'wmv', 'mp3', 'm4a',
-            'jpg', 'jpeg', 'mpga', 'webp', 'wma',
-        ],
         'max_upload_time' => 5,
     ],
-    
-    // This is important - disable asset injection if using Filament
-    'inject_assets' => false,
-    
-    'middleware_group' => 'web',
 ];
