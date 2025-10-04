42f6584a0a0b71d2f3ceda37021e69b9e8877aa2
kisaragiarya <lorpesarya@gmail.com>
2025-10-04 22:30:26 +0800

    add livewire routes

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+use Livewire\Livewire;
+
+Livewire::routes();
 
 Route::get('/', function () {
     return redirect()->route('filament.admin.auth.login');
