5caefa53435c6316cf5c2ade2b8cf72b7a353b76
kisaragiarya <lorpesarya@gmail.com>
2025-10-04 20:06:51 +0800

    please work

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Panel;
 
 class User extends Authenticatable
 {
@@ -38,6 +39,11 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true; // Allow all authenticated users for now
+    }
     protected function casts(): array
     {
         return [
